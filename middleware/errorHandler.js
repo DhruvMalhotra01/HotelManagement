2ff9de1385b6b51b5dcbd9183a2e78ba7aea4e7c
Bavanpreet <157589194+Bavanpreet@users.noreply.github.com>
2025-11-18 23:12:21 +0530

    Update errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -3,6 +3,6 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err);  // Log the error
   res.status(500).send('Something went wrong. Please try again later.');
-};
+  };
 
 module.exports = errorHandler;
